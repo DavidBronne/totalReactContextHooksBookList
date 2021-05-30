13780b6b4596308ac83c1f6576e168cb8910e5db
David Bronne <davidbronne@gmail.com>
2021-05-30 17:45:11 +0200

    Comp AddBook (form) + addBook fct

---
+++ src/components/BookForm.js
@@ -0,0 +1,25 @@
+import React, {useContext, useState} from 'react';
+import {BookContext} from '../contexts/BookContext'
+
+const BookForm = () => {
+    const {addBook} = useContext(BookContext)
+    const [title, setTitle] = useState()
+    const [author, setAuthor] = useState()
+
+    const hanldeSubmit = (e) => {
+        e.preventDefault()
+        addBook(title,author)
+        setTitle('');
+        setAuthor("")
+    }
+    
+    return (
+        <form onSubmit={hanldeSubmit} >
+            <input type="text" required placeholder='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+            <input type="text" required placeholder='Author' value={author} onChange={(e)=>setAuthor(e.target.value)}/>
+            <input type='submit' value='add Book'/>
+        </form>
+    );
+}
+ 
+export default BookForm;
