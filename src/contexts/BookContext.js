95d1ac3e0b57137cf19307d4f1128913ebc8e08e
David Bronne <davidbronne@gmail.com>
2021-05-31 09:58:14 +0200

    App refactored with useReducer

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,31 +1,15 @@
-import React, {createContext, useState} from 'react';
-import {v4 as uuidv4} from 'uuid';
+import React, {createContext, useReducer} from 'react';
+import BookReducer from '../reducers/BookReducer';
+
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
 
-    const [books, setBooks] = useState([
-        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-        {title: 'the final empire', author: 'brandon sanderson', id: 2}
-    ])
-    
-    const addBook = (title, author) => {
-        setBooks([
-            ...books,
-            {title:title, author:author, id:uuidv4()}
-        ])
-    }
-    const deleteBook = (id) => {
-        setBooks(
-            books.filter( book => {
-                return (book.id !== id)
-            })
-        )
-    }
-    
+    const [books, dispatch] = useReducer(BookReducer, [])
+ 
     return (
-        <BookContext.Provider value={{books, addBook, deleteBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     );
---
+++ src/reducers/BookReducer.js
@@ -0,0 +1,27 @@
+import {v4 as uuidv4} from 'uuid';
+
+const BookReducer = (state, action) => {
+    
+        switch (action.type) {
+            case 'ADD_BOOK':
+                return (
+                    [
+                        ...state,
+                        {title:action.book.title , author:action.book.author, id:uuidv4()}
+                    ]
+                )
+            case 'REMOVE_BOOK':
+                return (
+                    state.filter( book =>{
+                        return (
+                            book.id !== action.id
+                        )
+                    })
+                )
+            default: 
+                return state
+        }
+    
+}
+ 
+export default BookReducer;
